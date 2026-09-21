9a7d06cbea74750e7c95a2d28078fe14be9c4d63
Leonardo Pastrana <leopastra2004@gmail.com>
2026-09-21 02:30:37 -0500

    fixes security bug

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useSiteContent } from "../hooks/useSiteContent";
 
-const ADMIN_PASSWORD = import.meta.env.ADMIN_PASSWORD;
+const ADMIN_PASSWORD = import.meta.env.VITE_PANEL_NUMBER;
 
 export default function Admin() {
   const [content, saveContent] = useSiteContent();
